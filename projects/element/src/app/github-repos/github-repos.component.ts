8b20855637eeca90ef41d7327bb88a7da48590ee
tomastrajan <tomas.trajan@gmail.com>
2019-07-26 09:35:44 +0200

    fix(demo): broken stream after backend 404

--- projects/element/src/app/github-repos/github-repos.component.ts
+++ projects/element/src/app/github-repos/github-repos.component.ts
@@ -1,7 +1,14 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, Input, OnChanges, OnInit } from '@angular/core';
 import { BehaviorSubject, Observable, of } from 'rxjs';
-import { catchError, debounceTime, filter, map, switchMap, tap } from 'rxjs/operators';
+import {
+  catchError,
+  debounceTime,
+  filter,
+  map,
+  switchMap,
+  tap
+} from 'rxjs/operators';
 
 @Component({
   selector: 'element-github-repos',
@@ -24,9 +31,10 @@ export class GithubReposComponent implements OnInit, OnChanges {
       debounceTime(500),
       tap(username => console.log('[ELEMENT] debug username:', username)),
       switchMap(username =>
-        this.httpClient.get(`https://api.github.com/users/${username}/repos`)
+        this.httpClient
+          .get(`https://api.github.com/users/${username}/repos`)
+          .pipe(catchError(() => of([])))
       ),
-      catchError(() => of([])),
       map((res: any[]) => res.map(r => r.name)),
       tap(repos => console.log('[ELEMENT] debug response repos:', repos))
     );
